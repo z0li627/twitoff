102fa2d4e7f6bb5b98768ff07fd1ae203a8cdfe5
Zoltan Gaspar <zoltan.gaspar627@gmail.com>
2019-12-18 16:51:21 +0100

    some updates

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -1,7 +1,8 @@
 """Code for our app"""
 
-from flask import Flask
-from .models import DB
+from decouple import config
+from flask import Flask, render_template, request
+from .models import DB, User
 
 # make our app factory
 
@@ -9,13 +10,24 @@ def create_app():
     app = Flask(__name__)
 
     #add config for database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config("DATABASE_URL")
+
+    #stop tracking mods in SQLALCHEMY
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     #have the database know about the app
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return 'Welcome to Twitoff!'
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title = 'Reset', users=[])
+
     return app
 
--- TWITOFF/models.py
+++ TWITOFF/models.py
@@ -6,6 +6,9 @@ class User(DB.Model):
     """Twitter users"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String(15), nullable=False)
+    newest_tweet_id =  DB.Column(DB.BigInteger)
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
 
 
 
@@ -13,4 +16,10 @@ class Tweet(DB.Model):
     """Tweets we pull"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(280))
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
+    embedding = DB.Column(DB.PickleType, nullable=False)
+
+    def __repr__(self):
+        return '<Tweet {}>'.format(self.text)
