07fad0ca7e01751d57589900821c23a164342b51
Zoltan Gaspar <zoltan.gaspar627@gmail.com>
2019-12-19 18:40:08 +0100

    adding Procfile

--- TWITOFF/app.py
+++ TWITOFF/app.py
@@ -3,6 +3,7 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 # make our app factory
 
@@ -28,6 +29,22 @@ def create_app():
         DB.drop_all()
         DB.create_all()
         return render_template('base.html', title = 'Reset', users=[])
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/name', methods=['GET'])
+    def user(name=None, message=''):
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = "User {} successfully added.".format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = "Error adding {}: {}".format(name,e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                              message=message)
+
 
     return app
 
--- TWITOFF/twitter.py
+++ TWITOFF/twitter.py
@@ -11,3 +11,33 @@ TWITTER_AUTH.set_access_token(config('TWITTER_ACCESS_TOKEN'),
                              config('TWITTER_ACCESS_TOKEN_SECRET'))
 TWITTER = tweepy.API(TWITTER_AUTH)
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
+
+
+def add_or_update_user(username):
+    """Add or update a user and their tweets, or give error"""
+    try:
+        twitter_user=TWITTER.get_user(username)
+        db_user=(User.query.get(twitter_user.id) or
+                User(id=twitter_user.id, name=username))
+        DB.session.add(db_user)
+        tweets = twitter_user.timeline(count=200, exclude_replies=True,
+                                      include_rts=False, tweet_mode='extended',
+                                      since_id=db_user.newest_tweet_id)
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+        for tweet in tweets:
+            embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+            db_tweet = Tweet(id=tweet.id, text=tweet.full_text[:300],
+                            embedding=embedding)
+            db.user.tweets.append(db_tweet)
+            DB.session.add(db_tweet)
+    except Exception as e:
+        print('Error processing {}: {}'.format(username,e))
+        raise e
+    else:
+        DB.session.commit()
+
+
+
+
+
